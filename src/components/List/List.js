54ee33cf5848573d6c099f2d917c3329d4a983aa
Vlad <vladstamatin@yahoo.com>
2020-07-21 12:10:10 +0300

    commit

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -15,6 +15,8 @@ function List(props) {
               <Link key={elem.id} to={`/${title}/${elem.original_title}`}>
                 <li key={elem.id}>
                   <Movie img_path={elem.poster_path} title={elem.title} adult={elem.adult}/>
+                  <p>{elem.title}</p>
+                  <p>{elem.release_date}</p>
                 </li>
               </Link>
             )
